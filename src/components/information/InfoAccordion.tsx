a4455dcfd2a5a361d279c075b60a5cf3a001e660
byanrkh <abyan.raditya@icloud.com>
2026-07-24 21:18:00 +0700

    ACS

--- src/components/information/InfoAccordion.tsx
+++ src/components/information/InfoAccordion.tsx
@@ -141,7 +141,7 @@ const sections: Section[] = [
             Email: <b>acs.projectalkp4@gmail.com</b>
           </li>
           <li>
-            WhatsApp: <b>+62 856-9770-3733</b>
+            WhatsApp: <b>+62 856-9770-3733 (Gading)</b>
           </li>
         </ul>
       </div>
